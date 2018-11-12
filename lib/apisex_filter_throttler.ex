3a1c0aabfbbc046eca68eb181e2ef46018d1d025
Tangui <29804907+tanguilp@users.noreply.github.com>
2018-11-12 21:33:09 +0300

    Added helpers throttling functions

--- lib/apisex_filter_throttler.ex
+++ lib/apisex_filter_throttler.ex
@@ -1,18 +1,127 @@
 defmodule APISexFilterThrottler do
+  @behaviour Plug
+  @behaviour APISex.Filter
+
   @moduledoc """
-  Documentation for APISexFilterThrottler.
+  An `APISex.Filter` plug for API requests rate-limiting
+
+  This plug uses the [Exhammer](https://github.com/ExHammer/hammer) package as
+  for the backend. This library uses the token bucket algorithm, which means that
+  this plug is mainly suitable for limiting abuses, not for accurate rate limiting. By
+  default, an local ETS backend is launched on startup.
+
+  ## Plug options
+
+  - `key`: one of (no default value):
+  a `(Plug.Conn.t -> String.t | {String.t, non_neg_integer(), non_neg_integer()})`
+  function, taking in parameter the connection and returning either the key, or the
+  tuple `{key, scale, limit}`.
+  Note that the `APISexFilterThrottler.Functions` provides with out-of-the-box functions
+  - `scale`: the time window of the token bucket algorithm. No default value.
+  - `limit`: the maximum limit of the token bucket algorithm. No default value.
+  - `increment`: the increment of the token bucket algorithm (defaults to `1`)
+  - `backend`: Exhammer's backend, default to `nil`
+  - `set_filter_error_response`: if `true`, sets the HTTP status code to `429`.
+  If false, does not do anything. Defaults to `true`
+  - `halt_on_filter_failure`: if set to `true`, halts the connection and directly sends the
+  response. When set to `false`, does nothing and therefore allows dealing with the error
+  later in the code. Defaults to `true`
+
+  ##
   """
 
-  @doc """
-  Hello world.
+  @impl Plug
+  def init(opts) do
+    if opts[:key] == nil, do: raise "`key` is a mandatory parameter"
+    if opts[:scale] == nil, do: raise "`scale` is a mandatory parameter"
+    if opts[:limit] == nil, do: raise "`limit` is a mandatory parameter"
 
-  ## Examples
+    opts
+    |> Enum.into(%{})
+    |> Map.put_new(:increment, 1)
+    |> Map.put_new(:backend, nil)
+    |> Map.put_new(:set_filter_error_response, true)
+    |> Map.put_new(:halt_on_filter_failure, true)
+  end
 
-      iex> APISexFilterThrottler.hello()
-      :world
+  @impl Plug
+  def call(conn, opts) do
+    case filter(conn, opts) do
+      {:ok, conn} ->
+        conn
 
-  """
-  def hello do
-    :world
+      {:error, conn, reason} ->
+        conn =
+          if opts[:set_filter_error_response] do
+            set_error_response(conn, reason, opts)
+          else
+            conn
+          end
+
+        if opts[:halt_on_filter_failure] do
+          conn
+          |> Plug.Conn.send_resp()
+          |> Plug.Conn.halt()
+        else
+          conn
+        end
+    end
+  end
+
+  @impl APISex.Filter
+  def filter(conn, opts) do
+    {key, scale, limit} =
+      case get_filter_fun(opts[:key]).(conn) do
+        key when is_binary(key) ->
+          {key, opts[:scale], opts[:limit]}
+
+        {key, scale, limit} ->
+          {key, scale, limit}
+      end
+
+    case throttle(opts[:backend], key, scale, limit, opts[:increment]) do
+      {:allow, _count} ->
+        {:ok, conn}
+
+      {:deny, _limit} ->
+        {:ok, {_count, count_remaining, _ms_to_next_bucket, _created_at}} =
+          Hammer.inspect_bucket(key, scale, limit)
+
+        {:error, conn, %APISex.Filter.Forbidden{filter: __MODULE__,
+                                                reason: :rate_limited,
+                                                error_data: count_remaining}}
+
+      {:error, reason} ->
+        {:error, conn, %APISex.Filter.Forbidden{filter: __MODULE__, reason: reason}}
+    end
+  end
+
+  defp get_filter_fun(fun) when is_function(fun, 1), do: fun
+  defp get_filter_fun(params) when is_list(params)
+  do
+    function_name = "throttle_" <>
+      (
+        params
+        |> Enum.sort()
+        |> Enum.map(&Atom.to_string/1)
+        |> Enum.join("_")
+      )
+
+    Module.concat(APISexFilterThrottler.Functions, function_name)
+  end
+
+  defp throttle(nil, key, scale, limit, increment) do
+    Hammer.check_rate_inc(key, scale, limit, increment)
+  end
+
+  defp throttle(backend, key, scale, limit, increment) do
+    Hammer.check_rate_inc(backend, key, scale, limit, increment)
+  end
+
+  @impl APISex.Filter
+  def set_error_response(conn, %APISex.Filter.Forbidden{error_data: count_remaining}, _opts) do
+    conn
+    |> Plug.Conn.put_resp_header("retry-after", trunc(count_remaining / 1000) + 1)
+    |> Plug.Conn.resp(:too_many_requests, "")
   end
 end
---
+++ lib/apisex_filter_throttler/functions.ex
@@ -0,0 +1,151 @@
+defmodule APISexFilterThrottler.Functions do
+  @doc """
+  Returns the IP address as a string
+
+  Make sure that the `remote_ip` of the `Plug.Conn.t` is correctly set
+
+  Example:
+  ```elixir
+  iex> throttle_by_ip(conn)
+  "121.42.56.166"
+  ```
+  """
+  @spec throttle_by_ip(Plug.Conn.t) :: String.t
+  def throttle_by_ip(conn) do
+    List.to_string(:inet.ntoa(conn.remote_ip))
+  end
+
+  @doc """
+  Returns the IP address concatenated to the path as a string
+
+  Make sure that the `remote_ip` of the `Plug.Conn.t` is correctly set
+
+  Example:
+  ```elixir
+  iex> throttle_by_ip_path(conn)
+  "121.42.56.166/api/prices/eurusd"
+  ```
+  """
+  @spec throttle_by_ip_path(Plug.Conn.t) :: String.t
+  def throttle_by_ip_path(conn) do
+    List.to_string(:inet.ntoa(conn.remote_ip)) <> conn.request_path
+  end
+
+  @doc """
+  Returns the authenticated client as a string
+
+  Make sure that a client is authenticated by an `APISex.Authenticator` plug, otherwise
+  this function will raise an exception since you certainly don't want clients to be
+  throttled, but not unauthenticated accesses
+
+  Example:
+  ```elixir
+  iex> throttle_by_client(conn)
+  "client32187"
+  ```
+  """
+  @spec throttle_by_client(Plug.Conn.t) :: String.t
+  def throttle_by_client(conn) do
+    case APISex.client(conn) do
+      client when is_binary(client) ->
+        client
+
+      nil ->
+        raise "#{__MODULE__}: unauthenticated client, cannot throttle"
+    end
+  end
+
+  @doc """
+  Returns the authenticated client concatenated to the path as a string
+
+  Make sure that a client is authenticated by an `APISex.Authenticator` plug, otherwise
+  this function will raise an exception since you certainly don't want clients to be
+  throttled, but not unauthenticated accesses
+
+  Example:
+  ```elixir
+  iex> throttle_by_client_path(conn)
+  "client32187/api/prices/eurusd"
+  ```
+  """
+  @spec throttle_by_client_path(Plug.Conn.t) :: String.t
+  def throttle_by_client_path(conn) do
+    case APISex.client(conn) do
+      client when is_binary(client) ->
+        client <> conn.request_path
+
+      nil ->
+        raise "#{__MODULE__}: unauthenticated client, cannot throttle"
+    end
+  end
+
+  @doc """
+  Returns the IP address concatenated to client as a string. May be usefull when
+  dealing with OAuth2 public clients such as mobiles apps or SPAs, when many devices
+  share the same `client_id` and to limit the *global* volume of calls so as to
+  protect against, for instance, application faults triggering request storms
+
+  Make sure that the `remote_ip` of the `Plug.Conn.t` is correctly set
+
+  Example:
+  ```elixir
+  iex> throttle_by_ip_client(conn)
+  "121.42.56.166client10341"
+  ```
+  """
+  @spec throttle_by_ip_client(Plug.Conn.t) :: String.t
+  def throttle_by_ip_client(conn) do
+    List.to_string(:inet.ntoa(conn.remote_ip)) <> APISex.client(conn)
+  end
+
+  @doc """
+  Returns the subject concatenated to the client. Maybe be usefull when
+  dealing with OAuth2 public clients such as mobiles apps or SPAs, when many devices
+  share the same `client_id`, to protect against a malicious user trying to globally
+  block the API
+
+  Example:
+  ```elixir
+  iex> throttle_by_subject_client(conn)
+  "bob23mymobileapp"
+  ```
+  """
+  @spec throttle_by_subject_client(Plug.Conn.t) :: String.t
+  def throttle_by_subject_client(conn) do
+    APISex.subject(conn) <> APISex.client(conn)
+  end
+
+  @doc """
+  Returns the IP address concatenated to subject and the client. Maybe be usefull when
+  dealing with OAuth2 public clients such as mobiles apps that can be used on several
+  personal devices (e.g. Android laptop, smartphone and tablet) simultaneously (however
+  devices could share the same IP address)
+
+  Example:
+  ```elixir
+  iex> throttle_by_ip_subject_client(conn)
+  "275.33.99.208bob23mymobileapp"
+  ```
+  """
+  @spec throttle_by_ip_subject_client(Plug.Conn.t) :: String.t
+  def throttle_by_ip_subject_client(conn) do
+    List.to_string(:inet.ntoa(conn.remote_ip)) <> APISex.subject(conn) <> APISex.client(conn)
+  end
+
+  @doc """
+  Same as throttle_by_subject_client/1 but avoids collisions by using `:erlang.phash2/1`
+
+  Example:
+  ```elixir
+  iex> throttle_by_ip_subject_client_safe(conn)
+  "37541545"
+  ```
+  """
+  @spec throttle_by_subject_client_safe(Plug.Conn.t) :: String.t
+  def throttle_by_subject_client_safe(conn) do
+    {conn.remote_ip, APISex.subject(conn), APISex.client(conn)}
+    |> :erlang.phash2()
+    |> Integer.to_string()
+  end
+
+end
